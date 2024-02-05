164bf5a51c881bbb0e4ab867028e8e62a1cb51db
K.M. Jones-Eastland <kmje405@gmail.com>
2024-02-05 11:21:45 -0700

    project init 2

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Purpose: To hold constants used throughout the application.
-export const SITE_TITLE = 'FOMAS';
-export const SITE_DESCRIPTION = 'Welcome to the Friends of Mountainair Animal Shelter Website';
-export const MAILING_ADDRESS = 'P.O. Box 565 Mountainair, NM 87036';
-export const PHONE_NUMBER = '505-437-1113';
