fa1a722a0ffd8ee4160eaab9e07081554ad73085
K.M. Jones-Eastland <kmje405@gmail.com>
2024-02-12 09:11:53 -0700

    Refactor footer component and add constants

---
+++ frontend/src/constants.ts
@@ -0,0 +1,3 @@
+export const mailingAddress = '1234 Main St, Anytown, USA';
+export const organizationFullName = 'Friends of Mountainair Animal Shelter'
+export const nonProfitStatus = 'We are a 501(c)3 non-profit organization'
