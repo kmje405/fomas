b217c409bcdf09450d1f35f7fb2e895994ad89a4
K.M. Jones-Eastland <kmje405@gmail.com>
2024-02-16 13:34:10 -0700

    removing mdx and m2dx

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,30 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import m2dx from "astro-m2dx";
 import react from "@astrojs/react";
 import { sanityIntegration } from "@sanity/astro";
 
-/** @type {import('astro-m2dx').Options} */
-const m2dxOptions = {
-  autoImports: true,
-  styleDirectives: true
-};
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx({
-    // If you have specific MDX options, they would go here
-  }), tailwind(), react(), sanityIntegration(
-    {
+  output: "hybrid",
+  integrations: [
+    tailwind(),
+    react(),
+    sanityIntegration({
       projectId: "1bs3a1kl",
       dataset: "production",
       useCdn: false,
-    }
-  )],
-
-  markdown: {
-    remarkPlugins: [[m2dx, m2dxOptions]],
-    extendDefaultPlugins: true
-  }
+      studioBasePath: "/admin",
+    })
+  ],
 });
