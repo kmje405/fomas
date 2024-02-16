18788c1f9c050d2b05d9f0fcf42b89327a25baed
K.M. Jones-Eastland <kmje405@gmail.com>
2024-02-16 13:33:51 -0700

    adding sanity.config.ts

---
+++ frontend/sanity.config.ts
@@ -0,0 +1,17 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {schemaTypes} from '../backend/schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'fomas-backend',
+
+  projectId: '1bs3a1kl',
+  dataset: 'production',
+
+  plugins: [structureTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
