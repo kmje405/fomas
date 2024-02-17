8426bdd5d64c713243e43b0572b6bcd3c08cd383
Oliver Speir <oliverspeir9@gmail.com>
2024-02-16 20:14:56 -0700

    remove .vercel and vscode settings

--- frontend/.vercel/output/_functions/chunks/index_7djSMXRg.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_IzPLWLU9.mjs').then(n => n.i);
-
-export { page };
--- frontend/.vercel/output/static/_astro/studio-component.PqLtGd5Q.js
+++
@@ -1,1 +0,0 @@
-import{at as p}from"./studio-component.EcszUpEI.js";import"./client.-OEVpZ6O.js";export{p as StudioComponent};
