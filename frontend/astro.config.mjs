a1fb659421d56bf1b816d72416d5176ad358a08f
Oliver Speir <oliverspeir9@gmail.com>
2024-02-16 20:18:51 -0700

    switch to netlify adapter and remove vscode settings

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,17 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import { sanityIntegration } from "@sanity/astro";
 
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: vercel(),
   integrations: [tailwind(), react(), sanityIntegration({
     projectId: "1bs3a1kl",
     dataset: "production",
     useCdn: false,
     studioBasePath: "/admin"
   })],
-  adapter: vercel()
+  adapter: netlify()
 });
