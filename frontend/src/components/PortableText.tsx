d0135e2a27c44027e0fefe11126341ac51a1afa7
K.M. Jones-Eastland <kmje405@gmail.com>
2024-02-18 13:44:11 -0700

    adding portable text to the project

--- frontend/src/components/PortableText.tsx
+++
@@ -1,7 +0,0 @@
-import {PortableText as PortableTextComponent} from '@portabletext/react';
-
-const PortableText = ({content}) => {
-  return <PortableTextComponent value={content} />;
-};
-
-export default PortableText;
