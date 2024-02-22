cb746817f48308b4632240c7a0769c589016a4bd
K.M. Jones-Eastland <kmje405@gmail.com>
2024-02-22 15:35:44 -0700

    changes to build

--- frontend/.netlify/functions-internal/ssr/chunks/generic_CL25hAUR.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_T7KUCfIq.mjs');
-
-export { page };
--- frontend/.netlify/functions-internal/ssr/chunks/index_IodsCmak.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_N9of6xHg.mjs').then(n => n.i);
-
-export { page };
