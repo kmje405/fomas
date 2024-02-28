7a82ff5bffcd541fb0954aace6576010abd1e20a
K.M. Jones-Eastland <kmje405@gmail.com>
2024-02-28 16:45:22 -0700

    Add frontend configuration files and dependencies

--- frontend/sanity.config.ts
+++ frontend/sanity.config.ts
@@ -1,18 +1,33 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
 import { netlifyTool } from 'sanity-plugin-netlify'
+import { dashboardTool } from "@sanity/dashboard";
+import { netlifyWidget } from "sanity-plugin-dashboard-widget-netlify";
 
-import {schemaTypes} from '../frontend/src/schemaTypes'
+import { schemaTypes } from '../frontend/src/schemaTypes'
 
 export default defineConfig({
   name: 'default',
   title: 'fomas-backend',
-
   projectId: '1bs3a1kl',
   dataset: 'production',
 
-  plugins: [structureTool(), netlifyTool()],
-
+  plugins: [structureTool(), netlifyTool(), dashboardTool({
+    widgets: [
+      netlifyWidget({
+        title: 'My Netlify deploys',
+        sites: [
+          {
+            title: 'FOMAS',
+            apiId: 'e4557a8a-2b79-4b43-8d2a-6daf997c251e',
+            buildHookId: '65dfbf229c4967863343b7be',
+            name: 'main--fomas',
+          }
+        ]
+      })
+    ]
+  })
+  ],
   schema: {
     types: schemaTypes,
   },
