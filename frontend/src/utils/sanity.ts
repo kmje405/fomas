4bc84a5cae889dbcdbfce0118d7cab431cf47ace
K.M. Jones-Eastland <kmje405@gmail.com>
2024-03-21 09:50:05 -0600

    Add animal profile page

--- frontend/src/utils/sanity.ts
+++ frontend/src/utils/sanity.ts
@@ -12,3 +12,18 @@ export async function getPost(slug:any) {
     },
   );
 }
+
+export async function getAnimal(slug:any) {
+  return await client.fetch(
+    `*[_type == "animal" && slug.current == $slug][0]`,
+    {
+      slug,
+    },
+  );
+}
+
+export async function getAnimals() {
+  return await client.fetch(
+    `*[_type == "animal" && defined(slug.current)] | order(_createdAt desc)`,
+  );
+}
