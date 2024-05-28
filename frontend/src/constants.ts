b74c53d73f327cbed65ec5d56b23cdfc6cd5a3fa
K.M. Jones-Eastland <kmje405@gmail.com>
2024-05-28 12:33:02 -0600

    updating and adding graphics to project

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,6 @@
 // Contact Information
 export const mailingAddress = "P.O. Box 565, Mountainair, NM 87036";
-export const organizationFullName = "The Friends of Mountainair Animal Shelter";
-export const nonProfitStatus ="The Friends of Mountainair Animal Shelter is a 501c3 nonprofit.";
+export const organizationFullName = "The Friends of the Mountainair Animal Shelter";
+export const nonProfitStatus = "The Friends of the Mountainair Animal Shelter is a 501(c)3 nonprofit.";
 export const phoneNumber = "(505) 437-1113";
 export const email = "woof@fomas.org";
