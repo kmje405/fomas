fc44cb347af69ecaa40a9052df91b723c1e0dfe2
K.M. Jones-Eastland <kmje405@gmail.com>
2024-05-29 15:22:33 -0600

    refactor: Update Onebytwo component to use altText prop for image alt attribute

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -4,3 +4,4 @@ export const organizationFullName = "The Friends of the Mountainair Animal Shelt
 export const nonProfitStatus = "The Friends of the Mountainair Animal Shelter is a 501(c)3 nonprofit.";
 export const phoneNumber = "(505) 437-1113";
 export const email = "woof@fomas.org";
+export const missionStatement = "The Friends of the Mountainair Animal Shelter (FoMAS) is a 501c3 nonprofit dedicated to improving the quality of life of all dogs who come to our shelter. Our mission strives to ensure that all dogs rescued by the Mountainair Animal Shelter receive the highest quality of care and are matched with the right families who welcome these dogs into their homes. We also provide the Mountainair community and surrounding areas with resources and information regarding animal welfare.";
