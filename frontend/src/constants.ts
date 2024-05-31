81808432bf3192988a1c1d6338b75a95608b017d
K.M. Jones-Eastland <kmje405@gmail.com>
2024-05-31 15:07:46 -0600

    refactor: Add adoption application URL constant

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -5,3 +5,4 @@ export const nonProfitStatus = "The Friends of the Mountainair Animal Shelter is
 export const phoneNumber = "(505) 437-1113";
 export const email = "woof@fomas.org";
 export const missionStatement = "The Friends of the Mountainair Animal Shelter (FoMAS) is a 501c3 nonprofit dedicated to improving the quality of life of all dogs who come to our shelter. Our mission strives to ensure that all dogs rescued by the Mountainair Animal Shelter receive the highest quality of care and are matched with the right families who welcome these dogs into their homes. We also provide the Mountainair community and surrounding areas with resources and information regarding animal welfare.";
+export const adoptionApplicationURL = "https://drive.google.com/file/d/1VnYmcYJDFgU0hQDxCnD8SBIRInN2Xqr7/view?usp=drive_link";
