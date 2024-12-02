15f98932460fd09a14d1de09918f1b512615db08
K.M. Jones-Eastland <kmje405@gmail.com>
2024-12-02 16:54:09 -0600

    updated build

--- frontend/.netlify/functions-internal/ssr/entry.mjs
+++ frontend/.netlify/functions-internal/ssr/entry.mjs
@@ -1,27 +1,27 @@
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_gXV6qHw9.mjs';
+import { manifest } from './manifest_Rug1A2XK.mjs';
 import * as serverEntrypointModule from '@astrojs/netlify/ssr-function.js';
 import { onRequest } from './_noop-middleware.mjs';
 
 const _page0 = () => import('./chunks/generic_utIRRhtG.mjs');
 const _page1 = () => import('./chunks/studio-route_fQcfTi0T.mjs');
-const _page2 = () => import('./chunks/404_zoOtoXyB.mjs');
-const _page3 = () => import('./chunks/about_5ngPXEBE.mjs');
-const _page4 = () => import('./chunks/adoption_v_ae5UDK.mjs');
-const _page5 = () => import('./chunks/_slug__Rrw2rOfI.mjs');
-const _page6 = () => import('./chunks/blog_qaOR-uI3.mjs');
-const _page7 = () => import('./chunks/director_HRE2XMZI.mjs');
-const _page8 = () => import('./chunks/president_y_ezdYoU.mjs');
-const _page9 = () => import('./chunks/secretary_-heZDH27.mjs');
-const _page10 = () => import('./chunks/treasurer_NFNtcICo.mjs');
-const _page11 = () => import('./chunks/vice-president_E_aAz8Ej.mjs');
-const _page12 = () => import('./chunks/index_QQBsqq2-.mjs');
-const _page13 = () => import('./chunks/contact_Lp1W8QSt.mjs');
-const _page14 = () => import('./chunks/dogs_yEMsAEYk.mjs');
-const _page15 = () => import('./chunks/donate_8zSfOfjb.mjs');
-const _page16 = () => import('./chunks/_slug__gZ0eVahC.mjs');
-const _page17 = () => import('./chunks/resources_biarU3PJ.mjs');
-const _page18 = () => import('./chunks/volunteer_-P1oqmtD.mjs');
+const _page2 = () => import('./chunks/404_O5Z8I3CC.mjs');
+const _page3 = () => import('./chunks/about_YnNqvQzJ.mjs');
+const _page4 = () => import('./chunks/adoption_PSVr2sfo.mjs');
+const _page5 = () => import('./chunks/_slug__UvOQu_3f.mjs');
+const _page6 = () => import('./chunks/blog_ozrr0ZlH.mjs');
+const _page7 = () => import('./chunks/director_xFTtuvnY.mjs');
+const _page8 = () => import('./chunks/president_Fal64ydH.mjs');
+const _page9 = () => import('./chunks/secretary_zVcQ07SD.mjs');
+const _page10 = () => import('./chunks/treasurer_n4aTUqkm.mjs');
+const _page11 = () => import('./chunks/vice-president_RND51dlw.mjs');
+const _page12 = () => import('./chunks/index_mnMSk46G.mjs');
+const _page13 = () => import('./chunks/contact_Co7uHXWl.mjs');
+const _page14 = () => import('./chunks/dogs_pTjimy8z.mjs');
+const _page15 = () => import('./chunks/donate_RWNzy_pj.mjs');
+const _page16 = () => import('./chunks/_slug__lAqxSKG6.mjs');
+const _page17 = () => import('./chunks/resources_GZZjXKkM.mjs');
+const _page18 = () => import('./chunks/volunteer_PhGJFkPb.mjs');
 const pageMap = new Map([
     ["node_modules/.pnpm/astro@4.3.2_@types+node@20.11.17_typescript@5.3.3/node_modules/astro/dist/assets/endpoint/generic.js", _page0],
     ["node_modules/.pnpm/@sanity+astro@2.2.0_@sanity+client@6.12.4_astro@4.3.2_@types+node@20.11.17_typescript@5.3.3___yygcdup7qxvzpquwopob4wh4ge/node_modules/@sanity/astro/dist/studio/studio-route.astro", _page1],
@@ -50,7 +50,7 @@ const _manifest = Object.assign(manifest, {
     middleware: onRequest
 });
 const _args = {
-    "middlewareSecret": "4758c93e-6a54-49bc-8e25-92d62d5946af"
+    "middlewareSecret": "f6e68306-6859-40d0-b93a-238d17cc91a7"
 };
 const _exports = serverEntrypointModule.createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
