de274838cd7519d9d0d91a81e7ade6bd5f74f337
K.M. Jones-Eastland <kmje405@gmail.com>
2024-12-02 16:00:03 -0600

    removing outdated code

--- frontend/.netlify/functions-internal/ssr/chunks/404_LgfFU-hF.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_V8GqmGWR.mjs').then(n => n._);
-
-export { page };
--- frontend/.netlify/functions-internal/ssr/chunks/studio-route_e9pGIkq6.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/studio-route_e2SH3Itv.mjs');
-
-export { page };
